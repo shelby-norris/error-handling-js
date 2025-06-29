6a8d71d52aa0f2d6c5267d791afdd37275f043a6
Shelby Norris <shelbs9109@gmail.com>
2025-06-28 20:58:53 -0500

    Completed all parts of assignment in js file

--- app.js
+++ app.js
@@ -14,11 +14,25 @@ function divideNumbers(a, b) {
   }
 }
 
-// divideNumbers(10, 5);
+divideNumbers(10, 5);
 
-// divideNumbers(4, 0);
+divideNumbers(4, 0);
 
 // Part 2: Error Handling in Asynchronous Code
+
+async function fetchData() {
+  try {
+    const response = await fetch("https://httpstat.us/500");
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log("ERROR: Something went wrong");
+  } finally {
+    console.log("Fetch attempt finished");
+  }
+}
+
+fetchData();
 
 // Part 3: Debugging JavaScript Errors
 // Correct the code so that it checks for properly undefined name and logs "Hello, Guest!" if no name is provided
@@ -27,7 +41,7 @@ function greet(name) {
   if (name === undefined) {
     console.log("Hello Guest!");
   } else {
-    console.log("Hello " + name + "!")
+    console.log("Hello " + name + "!");
   }
 }
 
